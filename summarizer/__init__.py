f172824eec833fb13810015aaedc562e5ef2f515
hicorelearningapp <hicorelearningapp@gmail.com>
2025-09-16 22:57:17 +0530

    Update __init__.py

--- summarizer/__init__.py
+++ summarizer/__init__.py
@@ -1,5 +1,10 @@
 # summarizer/__init__.py
 from .document_processor import DocumentProcessor
 from .common import SummarizerPipeline
+from .llm_summarizer import LLMSummarizer
 
-__all__ = ['DocumentProcessor', 'SummarizerPipeline']
+__all__ = [
+    "DocumentProcessor",
+    "SummarizerPipeline",
+    "LLMSummarizer"
+]
