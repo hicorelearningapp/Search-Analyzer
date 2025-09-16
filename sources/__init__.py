f20d3382896dbd136b3932297a03d9268341d1cb
hicorelearningapp <hicorelearningapp@gmail.com>
2025-09-16 22:25:36 +0530

    Update __init__.py

--- sources/__init__.py
+++ sources/__init__.py
@@ -1,22 +1,6 @@
 # sources/__init__.py
+from .pdf_loader import PDFSummarizer
+from .video_transcript import YouTubeTranscriptManager
+from .web_search import WebSearchManager
 
-try:
-    from .pdf_loader import PDFSummarizer
-except ImportError:
-    PDFSummarizer = None
-
-try:
-    from .video_transcript import YouTubeTranscriptManager
-except ImportError:
-    YouTubeTranscriptManager = None
-
-try:
-    from .web_search import WebSearchManager
-except ImportError:
-    WebSearchManager = None
-
-__all__ = [
-    "PDFSummarizer",
-    "YouTubeTranscriptManager",
-    "WebSearchManager"
-]
+__all__ = ["PDFSummarizer","YouTubeTranscriptManager","WebSearchManager"]
