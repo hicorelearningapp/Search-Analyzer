6d1890fb27f58db8df5c3d3ca6d5aa66da5b7123
hicorelearningapp <hicorelearningapp@gmail.com>
2025-09-16 18:18:17 +0530

    Update __init__.py

--- sources/__init__.py
+++ sources/__init__.py
@@ -1,1 +1,7 @@
+# sources/__init__.py
+from .pdf_loader import PDFSummarizer
+from .video_transcript import YouTubeTranscriptManager
+from .web_search import WebSearchManager
+
+__all__ = ["PDFSummarizer", "YouTubeTranscriptManager", "WebSearchManager"]
 
