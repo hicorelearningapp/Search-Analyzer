dff6d9b8a32d0a94003a26fa7243397f8086be49
hicorelearningapp <hicorelearningapp@gmail.com>
2025-09-24 11:28:10 +0530

    Delete summarizer directory

--- summarizer/__init__.py
+++
@@ -1,8 +0,0 @@
-# summarizer/__init__.py
-from .document_processor import DocumentProcessor
-from .llm_summarizer import LLMSummarizer
-
-__all__ = [
-    "DocumentProcessor",
-    "LLMSummarizer"
-]
