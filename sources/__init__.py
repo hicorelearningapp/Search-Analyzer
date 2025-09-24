6e21e1be24bb3169c16cf53f48bf4d589939de63
hicorelearningapp <hicorelearningapp@gmail.com>
2025-09-24 11:27:58 +0530

    Delete sources directory

--- sources/__init__.py
+++
@@ -1,6 +0,0 @@
-# sources/__init__.py
-from .pdf_loader import PDFManager
-from .video_transcript import YouTubeTranscriptManager
-from .web_search import WebSearchManager
-
-__all__ = ["PDFManager","YouTubeTranscriptManager","WebSearchManager"]
